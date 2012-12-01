0d94c1109a492891d715c82e54e95d9b8c22606d
Roman Roan <roman.roan@gmail.com>
2012-12-01 15:18:34 +1100

    Converted to ARC. Removed JSONKit dependency and using NSJSONSerialization in iOS5

--- KakaoLinkCenter.m
+++ KakaoLinkCenter.m
@@ -4,15 +4,14 @@
 // @version 2.0
 //
 #import "KakaoLinkCenter.h"
-#import "JSONKit.h"
 
 static NSString *StringByAddingPercentEscapesForURLArgument(NSString *string) {
-	NSString *escapedString = (NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+	NSString *escapedString = (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
 																				  (CFStringRef)string,
 																				  NULL,
 																				  (CFStringRef)@"!*'();:@&=+$,/?%#[]",
 																				  kCFStringEncodingUTF8);
-	return [escapedString autorelease];
+	return escapedString;
 }
 
 static NSString *HTTPArgumentsStringForParameters(NSDictionary *parameters) {
@@ -104,7 +103,10 @@ static NSString *const StoryLinkURLBaseString = @"storylink://posting";
 	if (!avalibleAppLink)
 		return NO;
     
-    NSString *appDataString = [[NSDictionary dictionaryWithObject:metaInfoArray forKey:@"metainfo"] JSONString];
+	NSData *jsonData = [NSJSONSerialization dataWithJSONObject:[NSDictionary dictionaryWithObject:metaInfoArray forKey:@"metainfo"]
+													   options:0
+														 error:nil];
+    NSString *appDataString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
     if ( appDataString == nil )
         return NO;
     
@@ -143,7 +145,10 @@ static NSString *const StoryLinkURLBaseString = @"storylink://posting";
 									   nil];
 	
 	if (urlInfoDict.count > 0) {
-		[parameters setObject:[urlInfoDict JSONString] forKey:@"urlinfo"];
+		NSData *jsonData = [NSJSONSerialization dataWithJSONObject:urlInfoDict
+														   options:0
+															 error:nil];
+		[parameters setObject:[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding] forKey:@"urlinfo"];
 	}
 	
 	return [self openStoryLinkWithParams:parameters];
